33f524df1dc553cf674b8c62f49bc78d1bc701ce
Admin-PC\Admin <len88ok@gmail.com>
2017-01-10 22:01:28 +0200

    popovers

---
+++ scripts/main.js
@@ -0,0 +1,95 @@
+
+$(document).ready(function(){
+    $("#bp-popover").popover({
+   content: function() {
+		          return $('#popover-content1').html();
+		        }
+		});
+		$('#google-popover').popover({
+	    content: function() {
+		          return $('#popover-content2').html();
+		        }
+		});
+		$('#nike-popover').popover({
+	    content: function() {
+		          return $('#popover-content3').html();
+		        }
+		});
+		$('#intel-popover').popover({
+	    content: function() {
+		          return $('#popover-content4').html();
+		        }
+		});
+		$('#mts-popover').popover({
+	    content: function() {
+		          return $('#popover-content5').html();
+		        }
+		});
+		$('#samsung-popover').popover({
+	    content: function() {
+		          return $('#popover-content6').html();
+		        }
+		});
+		$('#skype-popover').popover({
+	    content: function() {
+		          return $('#popover-content7').html();
+		        }
+		});
+		$('#pepsi-popover').popover({
+	    content: function() {
+		          return $('#popover-content8').html();
+		        }
+		});
+		$('#yandex-popover').popover({
+	    content: function() {
+		          return $('#popover-content9').html();
+		        }
+		});
+		$('#7up-popover').popover({
+	    content: function() {
+		          return $('#popover-content10').html();
+		        }
+		});
+		$('#eclipse-popover').popover({
+	    content: function() {
+		          return $('#popover-content11').html();
+		        }
+		});
+		$('#dell-popover').popover({
+	    content: function() {
+		          return $('#popover-content12').html();
+		        }
+		});
+		$('#kharkov-popover').popover({
+	    content: function() {
+		          return $('#popover-content13').html();
+		        }
+		});
+		$('#peppers-popover').popover({
+	    content: function() {
+		          return $('#popover-content14').html();
+		        }
+		});$('#lviv-popover').popover({
+	    content: function() {
+		          return $('#popover-content15').html();
+		        }
+		});
+		$('#rate-popover').popover({
+	    content: function() {
+		          return $('#popover-content16').html();
+		        }
+		});
+		$('#profi-popover').popover({
+	    content: function() {
+		          return $('#popover-content17').html();
+		        }
+		});
+				$('#057-popover').popover({
+	    content: function() {
+		          return $('#popover-content18').html();
+		        }
+		});
+		$(document).on("click", ".popover .close" , function(){
+        $(this).parents(".popover").popover('hide');
+    });
+});								
