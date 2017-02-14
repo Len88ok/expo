a0bb956410f4f68a29a3ac1137690aef130a66ae
Admin-PC\Admin <len88ok@gmail.com>
2017-02-14 16:38:42 +0200

    dropdown on hover

--- scripts/main.js
+++ scripts/main.js
@@ -152,6 +152,12 @@ $(document).ready(function(){
 			off();
 		});
 
+		$('ul.nav li.dropdown').hover(function() {
+		  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+		}, function() {
+		  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+		});
+
 });
 
 
