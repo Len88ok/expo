17b13608dcaa5a9ae7b903ca2db325cfa74b752f
Admin-PC\Admin <len88ok@gmail.com>
2017-02-14 16:40:26 +0200

    del

--- scripts/main.js
+++ scripts/main.js
@@ -151,13 +151,6 @@ $(document).ready(function(){
 		$(document).on("click", ".close" , function(){ 
 			off();
 		});
-
-		$('ul.nav li.dropdown').hover(function() {
-		  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-		}, function() {
-		  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-		});
-
 });
 
 
